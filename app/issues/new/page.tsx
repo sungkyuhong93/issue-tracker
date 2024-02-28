0279c63d561ab002b506b8b167c8ffc68e22a38c
Sung Kyu Hong <sungkyuhong93@gmail.com>
2024-02-28 17:18:35 -0500

    handle errors

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
-import React from 'react'
-import { TextField, Button } from '@radix-ui/themes';
+import React, { useState } from 'react'
+import { TextField, Button, Callout } from '@radix-ui/themes';
 import SimpleMde from "react-simplemde-editor";
 import { useForm, Controller } from "react-hook-form";
 import axios from 'axios';
@@ -16,25 +16,35 @@ interface IssueForm {
 const NewIssuePage = () => {
   const router = useRouter();
   const { register, control, handleSubmit } = useForm<IssueForm>();
+  const [error, setError] = useState('');
 
   return (
-    <form 
-      className='max-w-xl space-y-3' 
-      onSubmit={handleSubmit(async(data) => {
-        await axios.post('/api/issues', data);
-        router.push('/issues');
-      })}
-    >
-      <TextField.Root>
-        <TextField.Input placeholder='Title' {...register('title')} />
-      </TextField.Root>
-      <Controller 
-        name="description" 
-        control={control} 
-        render={({ field }) => <SimpleMde placeholder="Description" {...field} />}
-      />
-      <Button>Submit New Issue</Button>
-    </form>
+    <div className='max-w-xl'>
+      {error && <Callout.Root color="red" className="mb-5">
+          <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>}
+      <form 
+        className='space-y-3' 
+        onSubmit={handleSubmit(async(data) => {
+          try {
+            await axios.post('/api/issues', data);
+            router.push('/issues');
+          } catch(error) {
+            setError('An unexpected error occurred.')
+          }
+        })}
+      >
+        <TextField.Root>
+          <TextField.Input placeholder='Title' {...register('title')} />
+        </TextField.Root>
+        <Controller 
+          name="description" 
+          control={control} 
+          render={({ field }) => <SimpleMde placeholder="Description" {...field} />}
+        />
+        <Button>Submit New Issue</Button>
+      </form>
+    </div>
   )
 }
 
