78b68400db067448ebc889f858e3b25c2efd9ae7
Sung Kyu Hong <sungkyuhong93@gmail.com>
2024-02-28 15:40:09 -0500

    set up radix ui

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,6 +1,12 @@
+import { Button } from "@radix-ui/themes";
+
 const IssuesPage = () => {
   return (
-    <div>IssuesPage</div>
+    <div>
+      <Button>
+        New Issue
+      </Button>
+    </div>
   )
 }
 
